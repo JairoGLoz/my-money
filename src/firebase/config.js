178dc1be3b6f206699089a8970ea968ce71df775
Jairo Lozano <jairo25492@gmail.com>
2022-09-03 07:00:59 -0600

    Implement user sign up

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBo9Ou7TFc97JE0rqJnVKywP8IMI9bEgDk",
+    authDomain: "mymoney-1d7a3.firebaseapp.com",
+    projectId: "mymoney-1d7a3",
+    storageBucket: "mymoney-1d7a3.appspot.com",
+    messagingSenderId: "314987524509",
+    appId: "1:314987524509:web:399ed8a2a6cac4c259ff74"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export{projectFirestore, projectAuth}
