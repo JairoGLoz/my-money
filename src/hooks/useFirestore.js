2598a3f2cfb3eda1abb8253adec30bd32c25f8b9
Jairo Lozano <jairo25492@gmail.com>
2022-09-05 10:26:15 -0600

    Implement add transaction to firestore

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,68 @@
+import {useReducer, useEffect, useState} from "react";
+import {projectFirestore, timestamp} from "../firebase/config";
+
+let initialState = {
+    document: null, isPending: false, error: null, // will contain error form firebase if present
+    success: null
+}
+
+const firestoreReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADDED_DOCUMENT':
+            return {
+                isPending: false, document: action.payload, success: true, err: null
+            }
+        case 'ERROR':
+            return {
+                isPending: false, document: null, success: false, err: action.payload
+            }
+        case 'IS_PENDING':
+            return {
+                isPending: true, document: null, success: false, err: null
+            };
+        default:
+            return state
+    }
+}
+
+export const useFirestore = (collection) => {
+    // response is the response from firestore, not the response itself but one
+    // object of our own that represents such response.
+    const [response, dispatch] = useReducer(firestoreReducer, initialState)
+
+    const [isCancelled, setIsCancelled] = useState(false)
+
+    // collection ref
+    const ref = projectFirestore.collection(collection)
+
+    // only dispatch if not cancelled
+    const dispatchIfNotCancelled = (action) => {
+        if (!isCancelled) {
+            dispatch(action)
+        }
+    }
+
+    // Add a document
+    const addDocument = async (doc) => {
+        dispatch({type: 'IS_PENDING'})
+
+        try {
+            const createdAt = timestamp.fromDate(new Date())
+            const addedDocument = await ref.add({...doc, createdAt})
+            dispatchIfNotCancelled({type: 'ADDED_DOCUMENT', payload: addedDocument})
+        } catch (err) {
+            dispatchIfNotCancelled({type: 'ERROR', payload: err.message})
+        }
+    }
+
+    // Delete a document
+    const deleteDocument = (id) => {
+
+    }
+
+    useEffect(() => {
+        return () => setIsCancelled(true)
+    }, [])
+
+    return {addDocument, deleteDocument, response}
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,23 @@
 import styles from './Home.module.css'
 
+// Components
+import TransactionForm from "./TransactionForm";
+
+// hooks
+import {useAuthContext} from "../../hooks/useAuthContext";
+
 export default function Home() {
-    return(
-        <div>
-            Home
+
+    const {user} = useAuthContext();
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                transaction list
+            </div>
+            <div className={styles.sidebar}>
+                <TransactionForm uid={user.uid}/>
+            </div>
         </div>
     )
 }
